0f4c550f1bd2308f5ed0befb47032653637e618a
ilkaybohani <36853068+ilkaybohani@users.noreply.github.com>
2018-04-08 08:19:30 +0200

    Update index.js
    made changes in comparisons

--- week1/index.js
+++ week1/index.js
@@ -1,8 +1,6 @@
 
-console.log 
-("hello world!, Ciao mondo!, Hola mundo! ");
-
-
+console.log
+    ("hello world!, Ciao mondo!, Hola mundo! ");
 
 console.log("I'm awesome");
 
@@ -13,29 +11,24 @@ x = 5;
 console.log("value of x is going to be 5");
 console.log(x);
 
-
-var y="maher kosa";
+var y = "maher kosa";
 console.log("the value of y is maher kosa");
 console.log(y);
-y="bader dabbagh";
+y = "bader dabbagh";
 console.log("the value of y is going to be bader dabbagh");
 console.log(y);
 
-
-
-var z=7.25;
+var z = 7.25;
 console.log(z);
-var a=7;
+var a = 7;
 console.log(a);
-if (z>a){
-console.log(z)
-}else{
-console.log(a)
+if (z > a) {
+    console.log(z)
+} else {
+    console.log(a)
 }
 
-
-
-var things =[];
+var things = [];
 console.log("the value is unavailable");
 console.log(things);
 var animals = ["dogs", "cats", "monkeys"];
@@ -43,65 +36,55 @@ console.log(animals);
 animals.push("baby pig");
 console.log(animals);
 
-
-
-
-
-
 let myString = "this is a test";
 console.log(myString);
 var myStringLength;
 myStringLength = myString.length;
 console.log(myStringLength);
 
-
-
 var a = ("song");
 var x;
-var y =[5, 6, 7];
+var y = [5, 6, 7];
 var z = 9;
-console.log("The value of a is:" + a );
-console.log("The value of x is:" + undefined );
-console.log("The value of y is:" + [5, 6, 7] );
-console.log("The value of z is:" + 9 );
+console.log("The value of a is:" + a);
+console.log("The value of x is:" + undefined);
+console.log("The value of y is:" + [5, 6, 7]);
+console.log("The value of z is:" + 9);
 console.log("a is a string, x is undefined, y is an array, z is a number");
 console.log(typeof a);
 console.log(typeof x);
 console.log(typeof y);
 console.log(typeof z);
-if(typeof a == typeof x){
+if (typeof a === typeof x) {
     console.log("same type")
-} else{
+} else {
     console.log("not the same type!!!")
 }
-if(typeof a == typeof y){
+if (typeof a === typeof y) {
     console.log("same type")
-} else{
+} else {
     console.log("not the same type!!!")
 }
-if(typeof a == typeof z){
+if (typeof a === typeof z) {
     console.log("same type")
-} else{
+} else {
     console.log("not the same type!!!")
 }
-if(typeof x == typeof y){
+if (typeof x === typeof y) {
     console.log("same type")
-} else{
+} else {
     console.log("not the same type!!!")
 }
-if(typeof x == typeof z){
+if (typeof x === typeof z) {
     console.log("same type")
-} else{
+} else {
     console.log("not the same type!!!")
 }
-if(typeof y == typeof z){
+if (typeof y === typeof z) {
     console.log("same type")
-} else{
+} else {
     console.log("not the same type!!!")
 }
-
-
-
 
 var x = 7;
 x = x % 3;
@@ -120,19 +103,15 @@ d = d % 4;
 console.log("the new value of d is 3");
 console.log(d);
 
-
-
 var myArray = ["maher Kosa", 5];
-console.log(typeof "maher Kosa");
-console.log(typeof 5); 
+console.log(typeof myArray[0]);
+console.log(typeof myArray[1]);
 
-
-
-var a = 6/0;
-var b = 10/0;
+var a = 6 / 0;
+var b = 10 / 0;
 console.log(a);
 console.log(b);
-console.log(a == b);
+console.log(a === b);
 console.log(a === b);
 console.log(a !== b);
 
